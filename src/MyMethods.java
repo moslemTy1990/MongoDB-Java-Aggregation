cb4a262e1f728559f20a60048038a0e4093e5b48
moslem <m.teymoori.1990@gmail.com>
2020-01-11 22:03:11 +0100

    I added time bucket and get a time period into an array

--- src/MyMethods.java
+++ src/MyMethods.java
@@ -3,79 +3,69 @@ import com.mongodb.MongoClientURI;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
-import com.mongodb.client.model.Aggregates;
 import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.Projections;
-import com.mongodb.client.model.Sorts;
 import org.bson.Document;
 import org.bson.conversions.Bson;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.StreamSupport;
-
-import org.bson.Document;
-import org.bson.conversions.Bson;
-import com.mongodb.client.*;
-
-import static com.mongodb.client.model.Aggregates.match;
+import java.util.*;
 import static com.mongodb.client.model.Filters.*;
 
 public  class MyMethods {
-    public MongoClientURI MyMongoUri = new MongoClientURI("mongodb://etlReader:just3Xtr4ct@10.10.101.113:27017/sigit_his");
-    public MongoClient mongoClient = new MongoClient(MyMongoUri);
-    public List<String> dbs = new ArrayList<>();
-    public List<String> dbc = new ArrayList<>();
+    private MongoClientURI MyMongoUri = new MongoClientURI("mongodb://etlReader:just3Xtr4ct@10.10.101.113:27017/sigit_his");
+    private MongoClient mongoClient = new MongoClient(MyMongoUri);
+    private List<String> dbs = new ArrayList<>();
+    private List<String> dbc = new ArrayList<>();
+    private List<Long> timeBucketList = new ArrayList<>();
     MariaClass MariaDB = new MariaClass();
+
+    // checking the mongoConnection
     public void CheckConnection() {
         getDatabaseNames();
         System.out.println("The name of the data base: " + dbs.get(0));
-    }   // checking the mongoConnection
+    }
 
+    // geting the collection on a list
     public void getCollection() {
         System.out.println("the name of the collections of the data base: " + dbs.get(0) + "is" + getColectionNames());
-    }      // geting the collection on a list
+    }
 
     public void FilterTemp() {
 
+          long startTime = Long.parseLong("1571842449859");    //selecting a period
+          long finishTime = Long.parseLong("1574438049859");
 
-            long StartPeriod = Long.parseLong("1571842449859");    //selecting a period
-          //  long FinishTime = Long.parseLong("1571842519709");
+         createBucketTimePeriod(startTime,finishTime);  //fill up the time period bucket
 
         MongoCollection<Document> coll = mongoClient.getDatabase(dbs.get(0)).getCollection(dbc.get(2));
-        Bson filterID = Filters.eq("id", "144-12") ;   //Filtering a specific manufacturure
-        Bson TimeFilter= gte("lastTimestamp",StartPeriod);  // Filtering a period
+        Bson filterID = Filters.eq("id", "144-12") ;   //Filter based on a specific manufacturure
+        Bson startTimeFilter= gte("lastTimestamp",startTime);  // Filter based on a period
+        Bson endTimeFilter= lte("lastTimestamp",finishTime);  // Filter based on a period
         Bson filterType = Filters.type("signals", "array");     // Consider the array Signals
         Bson filterTypeXpr =  Filters.elemMatch("signals", Filters.eq("signal",
                 "InterfaceType.InjectionUnits.InjectionUnit_1.TemperatureZones.TemperatureZone_1.ActualTemperature"));
-       // Bson filterTypeXpr = expr(Document.parse("{ $gt: [ { $size: '$signals' }, 0 ] }")); // If signal has ni value avoid it.
-
+                    // Filtering a specific signal of the array
         List<Document> list = new ArrayList<>();   // in this part i apply the filter 
-        coll.find(and(TimeFilter,filterID,filterType, filterTypeXpr))
-                .limit(50)
+        coll.find(and(startTimeFilter,endTimeFilter,filterID,filterType, filterTypeXpr))
+                  // .limit(50)//Todo remove this limit , bucket data hour by hour , put into maria db each record representing each hour
+                 // store avg, store standard deviation, each in a record
                 .iterator()
                 .forEachRemaining(list::add);
 
-        for ( Document doc1: list) {        // in this part i get the value and store in the maria db through a method
+        for ( Document doc1: list) {        // in this part i get the value and the signal content and store in the maria db through a method
             List<Document> arr = (List<Document>) doc1.get("signals");
-            //arr.forEach(e -> System.out.println(e.get("value")));
             arr.forEach(e -> SaveToMariaDB(e.get("value").toString(),e.get("signal").toString()));
         }
-
-       FindIterable<Document>  doc=coll.find(and(TimeFilter,filterID,TimeFilter,filterType, filterTypeXpr)).limit(5);
+/*
+       FindIterable<Document>  doc=coll.find(and(startTimeFilter,endTimeFilter,filterID,startTimeFilter,filterType, filterTypeXpr)).limit(5);
         for (Document Document : doc) {
             System.out.println(Document);
         }
-
-
-                /*
-        TOD try to avoing result with empty signal and try to do a filter with specific signal
-        the get the values of the signal with start time and stop time
-        */   //ToDo
-
+  */
     }
 
+    /*
+    * SaveToMariaDB is a method that filters a specific signal
+    * and saves the Signal and its value to the database
+    */
     private void SaveToMariaDB(String val , String sig) {
         if(sig.equals("InterfaceType.InjectionUnits.InjectionUnit_1.TemperatureZones.TemperatureZone_1.ActualTemperature")) {
             System.out.println(val + " " + sig );
@@ -84,11 +74,36 @@ public  class MyMethods {
             }catch(Exception e){
                 System.out.println(e);
             }
+         }
+    }
 
-      }
+    /*
+    *CreateBucetTimePeriod is a method that fills up the BucketList
+    * in order to do filter aggregation based on a time period
+    */
+    private  void createBucketTimePeriod(Long startTime, Long finishtime) {
+        Calendar startDate = Calendar.getInstance();
+        Calendar endDate = Calendar.getInstance();
 
-    }   // this is the method to store to database
+        startDate.setTimeInMillis(startTime);
+        endDate.setTimeInMillis(finishtime);
 
+//        startDate.add(Calendar.DAY_OF_MONTH,30);
+//        System.out.println(startDate.getTime() + " " + startDate.getTimeInMillis());
+
+        try {
+            timeBucketList.add(startDate.getTimeInMillis());
+            do {
+                startDate.add(Calendar.HOUR,1);
+                timeBucketList.add(startDate.getTimeInMillis());
+            } while(startDate.getTime().before(endDate.getTime())) ;
+            System.out.println("Time Bucket Loaded");
+        }catch (Exception e){
+            System.out.println("Error : " + e.toString());
+        }
+    }
+
+    //list the database names and put them into an array
     private List<String> getDatabaseNames() {
         MongoCursor<String> dbsCursor = mongoClient.listDatabaseNames().iterator();
         while (dbsCursor.hasNext()) {
@@ -96,6 +111,8 @@ public  class MyMethods {
         }
         return dbs;
     }
+
+    //List the collection of the database and put them into an array
     private List<String> getColectionNames() {
         MongoCursor<String> Mycollections = mongoClient.getDatabase(dbs.get(0)).listCollectionNames().iterator();
         while (Mycollections.hasNext()) {
